5b1e0c6cea373026f109e8fde613b596b402a632
giftos1 <giftsydney676@gmail.com>
2024-03-03 23:06:02 +1000

    add PlaceCollection methods

--- placecollection.py
+++ placecollection.py
@@ -1,9 +1,44 @@
 """..."""
-
+import csv
+from operator import attrgetter
 
 # Create your PlaceCollection class in this file
 
 
 class PlaceCollection:
-    """..."""
-    pass
+
+    def __init__(self):
+        self.places = []
+
+    def load_places(self, file):
+        place_file = open(file, "r")
+        for each_row in place_file:
+            row = each_row.strip()
+            rows = row.split(",")  # split each row in file to a list
+            rows[2] = int(rows[2])  # convert number string to integer for sorting
+            self.places.append(rows)
+
+    def save_places(self):
+        place_file = open("places.csv", "w", newline='')
+        writer = csv.writer(place_file)
+        writer.writerows(self.places)  # write places in file using csv module
+        place_file.close()
+
+    def add_place(self, place):
+        return self.places.append(place)
+
+    def add_unvisited_place(self, unvisited_count=0):
+        for place in self.places:
+            if "n" in place[3]:
+                unvisited_count += 1
+        return unvisited_count
+
+    def sort(self, key, priority=None):
+        if priority is None:
+            return sorted(self.places, key=attrgetter(key))
+        else:
+            return sorted(self.places, key=attrgetter(key, priority))
+
+    def __repr__(self):
+        return repr(self.places)
+
