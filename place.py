4555d24e5bc6290814404d8c8360b2f982f1cc0f
giftos1 <giftsydney676@gmail.com>
2024-03-03 23:05:22 +1000

    add place methods

--- place.py
+++ place.py
@@ -14,12 +14,14 @@ class Place:
     def __str__(self):
         return f"{self.name} in {self.country} (priority {self.priority}) added to Travel Tracker"
 
-    def visited(self, visit_status="v"):
-        self.is_visited = visit_status
+    def not_visited(self, visit_status):
+        if not visit_status:
+            self.is_visited = "n"
         return self.is_visited
 
-    def not_visited(self, visit_status="n"):
-        self.is_visited = visit_status
+    def visited(self, visit_status):
+        if visit_status:
+            self.is_visited = "v"
         return self.is_visited
 
     def is_important(self, priority=2):
@@ -28,4 +30,6 @@ class Place:
         else:
             return "not important"
 
-    pass
+    def __repr__(self):
+        return '[{}, {}. {}, {}]'.format(self.name, self.country, self.priority, self.is_visited)
+
