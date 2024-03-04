49f3096370f1871f76a564dd649ba7bef319df1a
giftos1 <giftsydney676@gmail.com>
2024-03-04 18:56:57 +1000

    complete save places and add comments

--- placecollection.py
+++ placecollection.py
@@ -1,4 +1,6 @@
-"""..."""
+"""This PlaceCollection class contains a single attribute of places that is a list
+of place objects. It also contains methods - load places, save places, add place,
+sort and add unvisited places."""
 import csv
 from operator import attrgetter
 
@@ -11,8 +13,9 @@ from place import Place
 class PlaceCollection:
 
     def __init__(self):
-        self.places = []
+        self.places = []  # contains a list of Place objects
 
+    """load from csv file into Place objects in the list"""
     def load_places(self, file):
         place_file = open(file, "r")
         for each_row in place_file:
@@ -21,21 +24,19 @@ class PlaceCollection:
             rows[2] = int(rows[2])  # convert number string to integer for sorting
             self.places.append(Place(rows[0], rows[1], rows[2], rows[3]))  # appending instances to list
 
-    def save_places(self, file):
-        place_file = open(file, "w", newline='')
-        writer = csv.writer(place_file)
-        writer.writerows(self.places)  # write places in file using csv module
-        place_file.close()
-
+    """add a single Place object to the places attribute"""
     def add_place(self, place):
         return self.places.append(place)
 
-    def add_unvisited_place(self, unvisited_count=0):
+    """get number of unvisited places"""
+    def add_unvisited_places(self):
+        unvisited_count = 0
         for place in self.places:
-            if "n" in place[3]:
+            if place.is_visited == "n" or not place.is_visited:
                 unvisited_count += 1
         return unvisited_count
 
+    """sort by the key passed in then by priority"""
     def sort(self, key, priority=None):
         if priority is None:
             self.places = sorted(self.places, key=attrgetter(key))
@@ -44,5 +45,14 @@ class PlaceCollection:
             self.places = sorted(self.places, key=attrgetter(key, priority))
             return self.places
 
+    """save from place list into csv file"""
+    def save_places(self, file):
+        place_file = open(file, "w", newline='')
+        writer = csv.writer(place_file)
+        for place in self.places:
+            writer.writerow([place])  # write places in file using csv module
+        place_file.close()
+
+    """print a list of Place Objects"""
     def __repr__(self):
         return repr(self.places)
